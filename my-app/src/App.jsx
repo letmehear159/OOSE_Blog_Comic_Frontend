fff9c2cc606b5a285563855fb7a3a970f48aa2fc
leejahy25 <yyyuhaig2511@gmail.com>
2025-05-05 12:51:04 +0700

    Fix conflict

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Navbar from "./components/navigation/Navbar";
-import Sidebar from "./components/navigation/Sidebar";
+import { useContext, useEffect, useState } from 'react'
+import './App.css'
+import { AuthContext } from './context/auth.context.jsx'
+import { fetchAccount } from './services/userService.js'
+import { Button, message, Spin } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0);
+function App () {
+  const { user, setUser, isLoading, setIsLoading } = useContext(AuthContext)
 
+  useEffect(() => {
+    if (localStorage.getItem('access_token')) {
+      setIsLoading(true)
+      const userPromise = fetchAccount()
+      userPromise
+        .then(user => {
+          setUser(user)
+        })
+        .catch(error => {
+          setUser(null)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    }
+  }, [])
+
+  console.log(">>> Check user ",user)
   return (
-    <>
-      
-    </>
-  );
+    isLoading === true ? <Spin/> :
+      <>
+        Lấy dữ liệu thành công
+      </>
+  )
 }
 
-export default App;
+export default App
